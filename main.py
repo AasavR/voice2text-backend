2df0a5b16486251fb482ea449e7da0de45eb7fba
aasavravi <aasavravi@gmail.com>
2025-05-05 16:09:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 #from auth import router as auth_router
 #from wallet import router as wallet_router
-from .transcribe import router as transcribe_router
+from transcribe import router as transcribe_router
 
 from fastapi.staticfiles import StaticFiles
 import os
